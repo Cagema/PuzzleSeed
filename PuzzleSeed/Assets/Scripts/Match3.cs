15795b81f3851f98b480c8924f6a6fcecd49506e
HOME-PC\Valera <Cagema.erm@gmail.com>
2021-02-11 15:24:03 +0300

    Create game board

---
+++ PuzzleSeed/Assets/Scripts/Match3.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match3 : MonoBehaviour
+{
+    public ArrayLayout BoardLayout;
+
+    [Header("UI Elements")]
+    public Sprite[] pieces;
+    public RectTransform gameBoard;
+
+    [Header("Prefabs")]
+    public GameObject nodePiece;
+
+    int width = 8;
+    int height = 8;
+    Node[,] board;
+
+    System.Random random;
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        string seed = GetRandomSeed();
+        random = new System.Random(seed.GetHashCode());
+
+        InitializeBoard();
+        VerifyBoard();
+        InstatiateBoard();
+    }
+
+    void InitializeBoard()
+    {
+        board = new Node[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                board[x, y] = new Node((BoardLayout.rows[y].row[x]) ? -1 : FillPiece(), new Point(x, y));
+            }
+        }
+    }
+
+    void VerifyBoard()
+    {
+        List<int> remove;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Point p = new Point(x, y);
+                int val = GetValueAtPoint(p);
+                if (val <= 0) continue;
+
+                remove = new List<int>();
+                while (IsConnected(p, true).Count > 0)
+                {
+                    if (!remove.Contains(val))
+                        remove.Add(val);
+                    SetValueAtPoint(p, NewValue(ref remove));
+                }
+            }
+        }
+    }
+
+    void InstatiateBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int val = board[x, y].value;
+                if (val <= 0) continue;
+                GameObject p = Instantiate(nodePiece, gameBoard);
+                NodePiece node = p.GetComponent<NodePiece>();
+                RectTransform rect = p.GetComponent<RectTransform>();
+                rect.anchoredPosition = new Vector2(32 + (64 * x), -32 - (64 * y));
+                node.Initialize(val, new Point(x, y), pieces[val - 1]);
+            }
+        }
+    }
+
+    List<Point> IsConnected(Point p, bool main)
+    {
+        List<Point> connected = new List<Point>();
+        int val = GetValueAtPoint(p);
+        Point[] directions =
+        {
+            Point.up,
+            Point.right,
+            Point.down,
+            Point.left
+        };
+
+        foreach (Point dir in directions) // Checking if there is 2 or more same shapes in the directions
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                Point check = Point.add(p, Point.mult(dir, i));
+                if (GetValueAtPoint(check) == val)
+                {
+                    line.Add(check);
+                    same++;
+                }
+            }
+
+            if (same > 1) // If there are 2 or more of the same shape in the direction the we know it is a match
+            {
+                AddPoints(ref connected, line); // Add these points to the overarching connected list
+            }
+        }
+
+        for (int i = 0; i < 2; i++) // Checking if we are in the middle of two of the same shapes
+        {
+            List<Point> line = new List<Point>();
+
+            int same = 0;
+            Point[] check = { Point.add(p, directions[i]), Point.add(p, directions[i + 2]) };
+            foreach(var next in check) // Check both sides of the pieces, if they are the same value, add them to the list
+            {
+                if (GetValueAtPoint(next) == val)
+                {
+                    line.Add(next);
+                    same++;
+                }
+            }
+
+            if (same > 1)
+            {
+                AddPoints(ref connected, line);
+            }
+        }
+
+        if (main) // Checks for other matches along the current match
+        {
+            for (int i = 0; i < connected.Count; i++)
+            {
+                AddPoints(ref connected, IsConnected(connected[i], false));
+            }
+        }
+
+        if (connected.Count > 0)
+        {
+            connected.Add(p);
+        }
+
+        return connected;
+    }
+
+    void AddPoints(ref List<Point> points, List<Point> add)
+    {
+        foreach (Point p in add)
+        {
+            bool doAdd = true;
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (points[i].Equals(p))
+                {
+                    doAdd = false;
+                    break;
+                }
+            }
+
+            if (doAdd) points.Add(p);
+        }
+    }
+
+    private int FillPiece()
+    {
+        int val = 1;
+        val = (random.Next(0, 100) / (100 / pieces.Length)) + 1;
+        return val;
+    }
+
+    int GetValueAtPoint(Point p)
+    {
+        if (p.x < 0 || p.x >= width || p.y < 0 || p.y >= height) return -1;
+        return board[p.x, p.y].value;
+    }
+
+    void SetValueAtPoint(Point p, int v)
+    {
+        board[p.x, p.y].value = v;
+    }
+
+    int NewValue(ref List<int> remove)
+    {
+        List<int> available = new List<int>();
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            available.Add(i + 1);
+        }
+        foreach (var i in remove)
+        {
+            available.Remove(i);
+        }
+
+        if (available.Count <= 0) return 0;
+        return available[random.Next(0, available.Count)];
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    string GetRandomSeed()
+    {
+        string seed = "";
+        string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdifghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
+        for(int i = 0; i < 20; i++)
+        {
+            seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+        }
+        return seed;
+    }
+
+    [System.Serializable]
+    public class Node
+    {
+        public int value; // 0 = blank, 1 = cube, 2 = sphere, 3 = cyllinder, 4 = pyramid, 5 = octahedron, -1 = hole
+        public Point index;
+
+        public Node(int v, Point i)
+        {
+            value = v;
+            index = i;
+        }
+    }
+}
